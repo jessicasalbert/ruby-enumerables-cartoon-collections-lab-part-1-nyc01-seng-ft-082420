ad993d894f46e03eabed6565fc531f097eeb125b
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-06 13:42:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |char|
-    puts char
+  array.each_with_index do |index, char|
+    puts "#{index}. #{char}"
   end
 end
